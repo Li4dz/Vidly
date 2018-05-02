d96e5ecfe79d84e0b6ead1d58bc2310f8862bdc1
Richard G <richard.rggdev@gmail.com>
2018-05-02 10:28:54 -0500

    Hierarchical data

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
 namespace Vidly.Dtos
 {
@@ -13,5 +14,6 @@ namespace Vidly.Dtos
         public bool IsSubscribedToNewsletter { get; set; }
         public byte MembershipTypeId { get; set; }
         public DateTime? Birthday { get; set; }
+        public MembershipTypeDto MembershipType { get; set; }
     }
 }
