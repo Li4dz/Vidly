554c9536cd9238637c3b2d3ecc7491f1eace5e68
Richard G <richard.rggdev@gmail.com>
2018-05-02 15:18:03 -0500

    Defense programming

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -22,12 +22,24 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+            //if (newRental.MoviesIds.Count == 0)
+            //    return BadRequest("No Movie Ids have been given.");
 
-            var movies = _context.Movies.Where(m => newRental.MoviesIds.Contains(m.Id));
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            //if (customer == null)
+            //    return BadRequest("CustomerId is not valid.");
+
+            var movies = _context.Movies.Where(m => newRental.MoviesIds.Contains(m.Id)).ToList();
+
+            //if (movies.Count != newRental.MoviesIds.Count)
+            //    return BadRequest("One or more MoviesIds are invalid");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental
